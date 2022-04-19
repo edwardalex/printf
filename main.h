eba709165426eaf81155feb96d4b3d4cedcc3e21
Phoebe Mwende <mwendephoebe25@gmail.com>
2022-04-19 21:16:14 +0300

    C - printf

--- main.h
+++ main.h
@@ -20,12 +20,20 @@ typedef struct functions
 	int (*f)(va_list);
 } ob_fun;
 /*PROTOTYPES*/
-int print_binary(va_list args);
-int print_character(char c);
-int _printf(const char *format, ...);
-int print_string(va_list args);
-int print_c(va_list args);
-int print_percent(va_list args);
-int print_integers(va_list args);
-int (*get_function(const char *format))(va_list);
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 #endif

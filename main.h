bd8f9086cccb67f7c51f162cb2bdb6fcfdfbac83
Phoebe Mwende <mwendephoebe25@gmail.com>
2022-04-19 21:50:58 +0300

    Update main.h

--- main.h
+++ main.h
@@ -1,36 +1,40 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
-#include <stddef.h>
 #include <stdlib.h>
-
+#include <stdio.h>
 /**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
- *
- * Return: int
- */
-typedef struct structprint
+ * struct fn - Struct fn
+ * @ob: The operator
+ * @type: The function associated
+*/
+typedef struct fn
 {
-	char *q;
-	int (*u)(char *format, va_list);
-} structype;
+char *ob;
+int (*type)(va_list var);
+} fn_t;
 
-int _putchar(char ch);
-int _puts(char *string);
-int printc(char *format, va_list);
-int printstr(char *format, va_list);
-int (*driver(char *format))(char *format, va_list);
-int _printf(char *format, ...);
-int printint(char *format, va_list pa);
-int integer(int number);
-int contadordigit(int number);
-int _abs(int number);
-int printpercent(char *format, va_list pa);
-int printhex(char *format, va_list);
-int printHEX(char *format, va_list);
-int printocta(char *format, va_list);
-int print_unsign(char *format, va_list);
+int _putchar(char *c);
+int _printf(const char *format, ...);
+int _printarg(char *c);
+char *change_base(unsigned int num, int base);
+int p_char(va_list args);
+int p_string(va_list args);
+int p_percent(va_list args __attribute__((unused)));
+int p_int(va_list args);
+int p_bin(va_list args);
+int p_rev(va_list args);
+char *_strcpy(char *dest, const char *str);
+int _strlen(char *s);
+int *print_formats(int i, char *copyfmt, va_list args);
+int p_unsigned(va_list args);
+int p_octal(va_list args);
+int p_hex_upper(va_list args);
+int p_hex_low(va_list args);
+char *change_hex_low(unsigned int num, int base);
+int p_string_esp(va_list args);
+char *replace_str(char *s);
+int p_add(va_list args);
+char *rot13(char *n);
+int p_rot13(va_list args);
 #endif

13c532616d07c325c022fad438ab6d99c40b2d87
edwardalex <olooedwards69@gmail.com>
2022-04-20 07:40:44 -0700

    Updates

--- main.h
+++ main.h
@@ -1,46 +1,23 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdarg.h> /*for varyadic functions*/
-#include <stdlib.h> /*for malloc and NULL*/
-#include <unistd.h> /*for write*/
-#include <limits.h> /* for test cases*/
-
-/**
- * struct special_cases - struct for the special cases
- * @match: the special character to match after find a percentage
- * @function: the associated function to be called in each specialcase
- */
-
-typedef struct special_cases
-{
-	char *match;
-	int (*function)(va_list);
-} spc_t;
-
-/*format and print data*/
-int _printf(const char *format, ...);
-
-/*writes the character c to stdout*/
+#ifndef HEADER_MAIN
+#define HEADER_MAIN
+#include <stdarg.h>
 int _putchar(char c);
-
-/*search for match and execute the function according to this*/
-int (*mod_character_s(const char *next, int dino))(va_list);
-
-/*prints a char*/
-int print_char(va_list c);
-
-/*prints a string */
-int print_string(va_list s);
-
-/*converts a string to an integer and prints its content.*/
-int print_number(va_list i);
-
-/*converts a string to an unsigned integer and prints its content*/
-int print_unsigned(va_list u);
-
-/*prints a string in reverse*/
-int print_reverse(va_list r);
-
+void _puts(char *str);
+int _strlen(char *s);
+int _printf(const char *format, ...);
+char *convert_to(char representation[], unsigned int num, int base);
+int function_manager(char c, va_list arg);
+int _switch(char c, va_list arg);
+int print_character(va_list arg);
+int print_sign(va_list arg, int base);
+int print_unsign(va_list arg, int base);
+int print_string(va_list arg);
+int print_ptr(va_list arg);
+int print_rot13(va_list arg);
+int print_rev(va_list arg);
+int print_base16_upper_lower(va_list arg, char *representation);
+int loop_format(va_list arg, const char *string);
+int call_function_manager(char aux, va_list arg);
+int check_percent(int *flag, char aux);
 #endif
 
